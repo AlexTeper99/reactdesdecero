24034d7bd96809841fca8b5f1aa0647949d80dbb
AlexTeper99 <ateper@fi.uba.ar>
2021-07-17 03:47:11 -0300

    4.3 Componentes funcionales y useState

--- src/Cupcake.js
+++ src/Cupcake.js
@@ -1,49 +1,40 @@
-import {Component} from "react";
 
-class Cupcacke extends Component{
-    constructor(props) {
-        super(props)
-        this.state = {
-            vendido: false
-        }
-        this.vender = this.vender.bind(this)
+
+import {useState} from "react"
+
+const Cupcake = ({sabor, color}) => {
+
+//useState nos va a permitir utilizar el estado en los componentes funcionales.
+//useState devuelve un array de dos elementos estado y funcion que actualiza el estado.Voy a almacenarlo en una variable (array). Por convecion el nombre de la funcion empieza con set.
+//estado puede ser de cualquier tipo. (number, objeto etc)
+//el useState recibe el valor por defecto de mi componente
+    const [vendido,setVendido] = useState(false)
+
+    //vender ahora va a ser una funcion
+    const vender = () => {
+        setVendido(true)
     }
 
-    vender(){
-        this.setState({vendido : true})
-    }
-    //tengo que aplicar el metodo al presionar el boton. Para eso utilizo el atributo onClick
-    render(){
-        return(
-            <div className="cupcake">
-              <h2>{this.props.color}</h2>
-              <p>{`Sabor:  ${this.props.sabor}`}</p>
-              <p><b>Estado: </b>{this.state.vendido ? "Vendido": "A la venta"}</p>
-                {
-                    !this.state.vendido && <button onClick={this.vender}>vender</button>
-                }
-            </div>
+    return(
+        <div className="cupcake">
+            <h2>{color}</h2>
+            <p>{`Sabor:  ${sabor}`}</p>
+            <p><b>Estado: </b>{vendido ? "Vendido": "A la venta"}</p>
+            {
+                !vendido && <button onClick={vender}>Vender</button>
+            }
+        </div>
 
-        )
-    }
+    )
 }
 
-/**
- * setState tiene que estar bindeado desde mi metodo.
- * Siempre que cree mis propios metodos debo hacer eso para que funcionen.
- */
-//importante: llamar al meotodo sin parentesis porque sino se ejecutaria automaticamente al leer esa linea de codigo y no al hacer click sobre el.
-/**
- * Renderizado condicional: Que solo aparezcan elementos en pantalla mediante condiciones.
-   Si no esta vendido && pongo el boton vender
-
- */
+export default Cupcake
 
 /**
- * El estado es independiente de cada componente.
- * Para poder modificar el estado necesito ejecutar un metodo.
- *
- * Para modificar el estado debo utilizar setState() y mandarle de parametro un objeto que va a reemplazar completamente al objeto estado inicial.
- * El metodo no va a funcionar si no hago el bind, es decir asignarle al atributo this.vender el metodo vender.
+useState en vez de recibir un objeto puede recibir cualquier tipo de dato.
+ Por ejemplo un booleano.
+ Si quiero recibir un objeto por ejemplo
+ 1) Si mi declaracion es: const [estado,setEstado] = useState({vendido: false})
+ Para acceder a sus atributos hago:
+ estado.vendido
  */
-export default Cupcacke
