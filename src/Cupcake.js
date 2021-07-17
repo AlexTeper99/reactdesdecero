8615bdaffe009eb40a8704877efc3f56dc625168
AlexTeper99 <ateper@fi.uba.ar>
2021-07-17 01:00:31 -0300

    3.5 El estado de los componentes

--- src/Cupcake.js
+++ src/Cupcake.js
@@ -1,15 +1,35 @@
 import {Component} from "react";
 
 class Cupcacke extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            vendido: false
+        }
+        //estamos creando un estado para nuestro componente. Este estado inicial es un objeto que puede contener todos los valores que yo quiera
+    }
+    //render se encarga de retornar los valores.
     render(){
+
         return(
             <div className="cupcake">
               <h2>{this.props.color}</h2>
               <p>{`Sabor:  ${this.props.sabor}`}</p>
+              <p><b>Estado: </b>{this.state.vendido ? "Vendido": "A la venta"}</p>
+                {
+                    !this.state.vendido && <button>vender</button>
+                }
             </div>
+
         )
     }
 }
-//para acceder a las props la sintaxis cambia y se utiliza la palabra reservada this.
+
+/**
+ * Renderizado condicional: Que solo aparezcan elementos en pantalla mediante condiciones.
+   Si no esta vendido && pongo el boton vender
+
+ */
+
 
 export default Cupcacke
